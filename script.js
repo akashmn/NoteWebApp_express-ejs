b5793654de4fd8ce7aad7c8b60eea1497d08854f
Akash M Nandan <akash003nandan@gmail.com>
2025-06-14 12:00:01 +0530

    more routes/ middleswares/error handling

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 import express from 'express'
 const app = express()
 
+// Middleware to log requests
+app.use((req, res, next) => {
+    console.log(`${req.method} request for '${req.url}'`)
+    next()
+})
+
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
 app.get('/about', (req, res) => {
     res.send('About Page')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Page')
+})
+
+// Catch-all route for 404 errors
+app.use((req, res) => {
+    res.status(404).send('404 Not Found')
+})
+
+//for handling errors
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
 })
 
 app.listen(3000, () => {
